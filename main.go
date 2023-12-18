aea526442d0642af9c6e16cd7c0de8b9ffa715c1
MoltenWolfCub <oliver@tomlins.org.uk>
2023-12-18 21:21:00 +0000

    drew a triangle
    setup shaders and a game loop to draw those shaders and handle exit
    events

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"strings"
 
 	"github.com/go-gl/gl/v3.3-core/gl"
 	"github.com/veandco/go-sdl2/sdl"
@@ -24,4 +25,103 @@ func main() {
 	gl.Init()
 	version := gl.GoStr(gl.GetString(gl.VERSION))
 	fmt.Println("OpenGL Version", version)
+
+	fragmentShaderSource :=
+		`
+		#version 330 core
+		out vec4 FragColor;
+
+		void main() {
+			FragColor = vec4(0.0f,0.5f,0.75f,1.0f);
+		}
+		`
+	fragmentShader := gl.CreateShader(gl.FRAGMENT_SHADER)
+	csource, free := gl.Strs(fragmentShaderSource)
+	gl.ShaderSource(fragmentShader, 1, csource, nil)
+	free()
+	gl.CompileShader(fragmentShader)
+	var status int32
+	gl.GetShaderiv(fragmentShader, gl.COMPILE_STATUS, &status)
+	if status == gl.FALSE {
+		var logLength int32
+		gl.GetShaderiv(fragmentShader, gl.INFO_LOG_LENGTH, &logLength)
+		log := strings.Repeat("\x00", int(logLength+1))
+		gl.GetShaderInfoLog(fragmentShader, logLength, nil, gl.Str(log))
+		panic("Failed to compile fragment shader:\n" + log)
+	}
+
+	vertexShaderSource :=
+		`
+		#version 330 core
+		layout (location = 0) in vec3 aPos;
+
+		void main() {
+			gl_Position = vec4(aPos.x, aPos.y,aPos.z,1.0);
+		}
+		`
+	vertexShader := gl.CreateShader(gl.VERTEX_SHADER)
+	csource, free = gl.Strs(vertexShaderSource)
+	gl.ShaderSource(vertexShader, 1, csource, nil)
+	free()
+	gl.CompileShader(vertexShader)
+	gl.GetShaderiv(vertexShader, gl.COMPILE_STATUS, &status)
+	if status == gl.FALSE {
+		var logLength int32
+		gl.GetShaderiv(vertexShader, gl.INFO_LOG_LENGTH, &logLength)
+		log := strings.Repeat("\x00", int(logLength+1))
+		gl.GetShaderInfoLog(vertexShader, logLength, nil, gl.Str(log))
+		panic("Failed to compile vertex shader:\n" + log)
+	}
+
+	shaderProgram := gl.CreateProgram()
+	gl.AttachShader(shaderProgram, vertexShader)
+	gl.AttachShader(shaderProgram, fragmentShader)
+	gl.LinkProgram(shaderProgram)
+	var success int32
+	gl.GetProgramiv(shaderProgram, gl.LINK_STATUS, &success)
+	if success == gl.FALSE {
+		var logLength int32
+		gl.GetShaderiv(shaderProgram, gl.INFO_LOG_LENGTH, &logLength)
+		log := strings.Repeat("\x00", int(logLength+1))
+		gl.GetProgramInfoLog(shaderProgram, logLength, nil, gl.Str(log))
+		panic("Failed to link program:\n" + log)
+	}
+	gl.DeleteShader(vertexShader)
+	gl.DeleteShader(fragmentShader)
+
+	verticies := []float32{
+		-0.5, -0.5, 0.0,
+		0.5, -0.5, 0.0,
+		0.0, 0.5, 0.0,
+	}
+	var VBO uint32
+	gl.GenBuffers(1, &VBO)
+	gl.BindBuffer(gl.ARRAY_BUFFER, VBO)
+	var VAO uint32
+	gl.GenVertexArrays(1, &VAO)
+	gl.BindVertexArray(VAO)
+
+	gl.BufferData(gl.ARRAY_BUFFER, len(verticies)*4, gl.Ptr(verticies), gl.STATIC_DRAW)
+
+	gl.VertexAttribPointer(0, 3, gl.FLOAT, false, 3*4, nil)
+	gl.EnableVertexAttribArray(0)
+	gl.BindVertexArray(0)
+
+	for {
+		for event := sdl.PollEvent(); event != nil; event = sdl.PollEvent() {
+			switch event.(type) {
+			case *sdl.QuitEvent:
+				return
+			}
+		}
+
+		gl.ClearColor(0.0, 0.0, 0.0, 0.0)
+		gl.Clear(gl.COLOR_BUFFER_BIT)
+
+		gl.UseProgram(shaderProgram)
+		gl.BindVertexArray(VAO)
+		gl.DrawArrays(gl.TRIANGLES, 0, 3)
+
+		window.GLSwap()
+	}
 }
